99d0a9ec9da7dbef48e9f3c05b899afb747c6cf5
advanikhushbu <advanikhushbu@gmail.com>
2024-04-20 18:22:03 -0400

    Initial setup

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-recipes-component',
+    templateUrl: './recipes.component.html',
+    styleUrls: ['./recipes.component.scss']
+})
+
+export class Recipes {
+
+}
