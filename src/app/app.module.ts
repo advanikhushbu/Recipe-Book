8214dd40374fac69c062f1777f87c2fcd1138a83
Khushie <khushie@Pratiks-MacBook-Pro.local>
2024-04-21 00:08:37 -0400

    Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { Recipes } from './recipes/recipes.component';
+import { RecipeList } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    Recipes
+    Recipes,
+    RecipeList,
+    RecipeDetail,
+    RecipeItem,
+    ShoppingEdit,
+    ShoppingList
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,11 @@
+import { Component  } from "@angular/core";
+
+@Component({
+    selector:'app-recipe-item',
+    templateUrl:'./recipe-item.component.html',
+    styleUrls:['./recipe-item.component.scss']
+})
+
+export class RecipeItem {
+    
+}
