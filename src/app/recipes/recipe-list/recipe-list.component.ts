07210224b78aaffb1d75ff4e40e4521133490c07
Khushie <khushie@Pratiks-MacBook-Pro.local>
2024-04-21 23:20:27 -0400

    Basic Component Display Setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import { Recipe } from '../recipe.model';
 @Component({
     selector:'app-recipe-list',
     templateUrl:'./recipe-list.component.html',
@@ -7,5 +7,9 @@ import { Component } from "@angular/core";
 })
 
 export class RecipeList {
-
+    recipes: Recipe[] = [
+        new Recipe('Recipe 1', 
+        'This is Recipe 1', 
+        'https://www.foodandwine.com/thmb/fjNakOY7IcuvZac1hR3JcSo7vzI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/FAW-recipes-pasta-sausage-basil-and-mustard-hero-06-cfd1c0a2989e474ea7e574a38182bbee.jpg')
+    ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
     selector:'app-shopping-list',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
 })
 
 export class ShoppingList {
-    
+    ingredients: Ingredients[] = [
+        new Ingredients('Banana', 10),
+        new Ingredients('Apple', 2)
+    ]
 }
